3ab99280742db3871c7a056a0d095b9f73b6cd3e
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-07-24 10:24:05 -0600

    inicio

---
+++ gsmc20240724/src/main/java/com/gsmc20240724/gsmc20240724/Gsmc20240724Application.java
@@ -0,0 +1,13 @@
+package com.gsmc20240724.gsmc20240724;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gsmc20240724Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gsmc20240724Application.class, args);
+	}
+
+}
---
+++ gsmc20240724/src/test/java/com/gsmc20240724/gsmc20240724/Gsmc20240724ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsmc20240724.gsmc20240724;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gsmc20240724ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
